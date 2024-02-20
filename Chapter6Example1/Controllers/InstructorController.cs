f6bb627d5c83fae4908a10a844921785d9144b25
KnowenObiso <knowen.obiso@turnkeyafrica.com>
2024-02-20 19:33:26 +0300

    Adding Controllers and Views

---
+++ Chapter6Example1/Controllers/InstructorController.cs
@@ -0,0 +1,28 @@
+﻿using Chapter6Example1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Chapter6Example1.Controllers
+{
+    public class InstructorController : Controller
+    {
+       
+        SchoolDbContext db;
+        public InstructorController(SchoolDbContext db) {
+            this.db=db;
+
+        }
+        public async Task<IActionResult> AllInstructor()
+        {
+            var instructor = await db.Instructors.ToListAsync();
+            return View(instructor);
+        }
+        public IActionResult AddInstructor()
+        {
+            return View();
+        }
+    }
+}
+
