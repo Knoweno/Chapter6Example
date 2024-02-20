aabd427af05b4cc81427ee9a374ae08f52302b32
KnowenObiso <knowen.obiso@turnkeyafrica.com>
2024-02-20 20:40:39 +0300

    update3

--- Chapter6Example1/Program.cs
+++ Chapter6Example1/Program.cs
@@ -1,7 +1,14 @@
+using Chapter6Example1.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//Db connection
+var connectionString = builder.Configuration.GetConnectionString("SchoolDbContextConnection");
+builder.Services.AddDbContext<SchoolDbContext>(options =>
+       options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -12,6 +19,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
