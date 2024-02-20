028b6e8690f047377ec2ef94ebf8472f52d7a56e
KnowenObiso <knowen.obiso@turnkeyafrica.com>
2024-02-20 22:27:39 +0300

    update5

--- Chapter6Example1/Controllers/CourseController.cs
+++ Chapter6Example1/Controllers/CourseController.cs
@@ -16,18 +16,30 @@ namespace Chapter6Example1.Controllers
         }
         public async Task<IActionResult> AllCourse()
         {
-            var courrse = await db.Courses.Include(c=>c.Instructor).ToListAsync();
-            return View(courrse);
+            var course = await db.Courses.Include(c=>c.Instructor).ToListAsync();
+            return View(course);
         }
-
+        
         public async Task<IActionResult> AddCourse()
         {
             var instructorDisplay = await db.Instructors.Select(
                 x => new { Id = x.InstructorId, value = x.InstructorName }).ToListAsync();
             CourseAddCourseViewModel vm = new CourseAddCourseViewModel();
-            vm.InstructorList = new SelectList(instructorDisplay, "Id", "valie");
+            vm.InstructorList = new SelectList(instructorDisplay, "Id", "value");
             return View(vm);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCourse(CourseAddCourseViewModel vm)
+        {
+            var instructor = await db.Instructors.SingleOrDefaultAsync(i=>
+            i.InstructorId == vm.Instructor.InstructorId);
+            vm.Course.Instructor = instructor;
+            db.Add(vm.Course);
+            await db.SaveChangesAsync();
+            return RedirectToAction("AllCourse");
+        }
+        
 
     }
 }
